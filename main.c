aa55c1b6956175fca4101e5adc3f76acaad4f606
saiashish90 <saiashish90@gmail.com>
2018-07-11 14:33:26 +0530

    V2.With data stored with pipes as delimiter

--- main.c
+++ main.c
@@ -1,89 +1,123 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-enum marital{Single, Married, Divorced};
-
+#include <string.h>
 struct employee
 {
-	char name[25],address[50],phone[9],email[25],occupation[15],aadhar[11],birthplace[15];
+	char name[25],address[50],phone[11],email[25],occupation[15],aadhar[13],birthplace[15],m_status[10],income[10];
 	int code;
-	//enum marital m_status;
 	struct date
 	{
-		int dd,mm,yy;
+		char dd[3],mm[3],yy[5];
 	} dob;
 };
 
 void input(struct employee *emp,FILE *fp)
 {
-	int flag;
-    
-   	fp = fopen("emp.dat","a");
-	
-    	if (fp==NULL)
-    	{
-		fprintf(stderr,"\nError opend file\n");
-		exit(1);
-    	}
+	int flag,a;
+  fp = fopen("emp.dat","a");
+	if (fp==NULL)
+		{
+			fprintf(stderr,"\nError opend file\n");
+			exit(1);
+    }
 
-    	printf("Enter the following details\n");
-    
-    	printf("Name\n");
-    	scanf("%s",emp->name);
-    
-    	printf("Phone No.\n");
-    	scanf("%s",emp->phone);
-    
-    	printf("Aadhar no.\n");
-    	scanf("%s",emp->aadhar);
-    
-    	printf("Address \n");
-    	scanf("%s",emp->address);
-    
-    	printf("Occupation \n");
-    	scanf("%s",emp->occupation);
-    
-    	printf("Date of Birth in DD MM YYYY format\n");
-	scanf("%d%d%d",&(emp->dob.dd),&(emp->dob.mm),&(emp->dob.yy));
-    
-/*
-    	printf("Marital Status\n");
-    	printf("\t1.Single\n\t2.Married\n\t3.Divorced");
-    	scanf("%d",&(emp->m_status));
-*/    
-    	flag = fwrite(emp,sizeof(struct employee),1,fp);
-    
-    	if (flag != 0)
-        printf("Written Successfully");
-    	else
-        printf("Error/n");
-    	fclose(fp);    
+	printf("Enter the following details\n");
+
+	printf("Name\n");
+	scanf("%s",emp->name);
+	fprintf(fp,"%s|",emp->name );
+
+	printf("Phone No.\n");
+  scanf("%s",emp->phone);
+	fprintf(fp,"%s|",emp->phone );
+
+  printf("Aadhar no.\n");
+  scanf("%s",emp->aadhar);
+	fprintf(fp,"%s|",emp->aadhar );
+
+	printf("Date of Birth in DD MM YYYY format\n");
+	scanf("%s%s%s",emp->dob.dd,emp->dob.mm,emp->dob.yy);
+	fprintf(fp,"%s|%s|%s|",(emp->dob.dd),(emp->dob.mm),(emp->dob.yy));
+
+  printf("Address \n");
+  scanf("%s",emp->address);
+	fprintf(fp,"%s|",emp->address );
+
+  printf("Occupation \n");
+  scanf("%s",emp->occupation);
+	fprintf(fp,"%s|",emp->occupation );
+
+	printf("Marital status\n1.Single\n2.Married\n3.Divorced\n");
+	scanf("%d",&a );
+	if(a==1)
+	strcpy(emp->m_status,"Single");
+	else if(a==2)
+	strcpy(emp->m_status,"Married");
+	else if(a==3)
+	strcpy(emp->m_status,"Divorced");
+	fprintf(fp,"%s|",emp->m_status );
+
+	printf("Annual Income\n");
+	scanf("%s",(emp->income));
+	fprintf(fp,"%s|",emp->income );
+
+	fprintf(fp,"\n");
+
+
+
+
+  /*flag = fwrite(emp,sizeof(struct employee),1,fp);
+  if (flag != 0)
+  	printf("Written Successfully");
+  else
+  	printf("Error/n");*/
+  //fclose(fp);
 }
-	
+
 
 void read(struct employee *emp,FILE *fp)
 {
+
 	fp = fopen("emp.dat","r");
-	
+
 	if (fp==NULL)
 	{
 		fprintf(stderr,"\nError opend file\n");
 		exit(1);
-	}	
-	while(fread(emp,sizeof(struct employee),1,fp))
-	{ 
-		printf("Name:%s \n",emp->name);
-		printf("Phone number:%s \n",emp->phone);
-		printf("Aadhar no:%s \n",emp->aadhar);
-		printf("Address:%s \n",emp->address);
-		printf("Occupation:%s \n",emp->occupation);
-		//printf("%sMarital status: \n",emp->m_status);
 	}
-	
+
+	fscanf(fp,"%[^|]|s",emp->name);
+	printf("Name:%s\n",emp->name);
+
+	fscanf(fp,"%[^|]|s",emp->phone);
+	printf("Phone no:%s\n",emp->phone);
+
+	fscanf(fp,"%[^|]|s",emp->aadhar);
+	printf("Aadhar no:%s\n",emp->aadhar);
+
+	fscanf(fp,"%[^|]|s",emp->dob.dd);
+	fscanf(fp,"%[^|]|s",emp->dob.mm);
+	fscanf(fp,"%[^|]|s",emp->dob.yy);
+
+	printf("DOB:%s/%s/%s\n",emp->dob.dd,emp->dob.mm,emp->dob.yy);
+
+	fscanf(fp,"%[^|]|s",emp->address);
+	printf("Address:%s\n",emp->address);
+
+	fscanf(fp,"%[^|]|s",emp->occupation);
+	printf("Occupation:%s\n",emp->occupation);
+
+	fscanf(fp,"%[^|]|s",emp->m_status);
+	printf("Marital status:%s\n",emp->m_status);
+
+	fscanf(fp,"%[^|]|s",emp->income);
+	printf("Income:%s\n",emp->income);
+
+
 	fclose(fp);
 }
 void main()
-{	/*
+{
 	FILE *fp;
 	printf("Enter 1 for input and 2 for read\n");
 	int choice;
@@ -91,23 +125,22 @@ void main()
 	//struct employee *emp;
 	struct employee emp;
 	while(choice==1)
-	{		
+	{
 		input(&emp,fp);
 		printf("Do you want to continue inputting");
 		scanf("%d",&choice);
 	}
-	
-	if (choice == 2)
-	{ 	
-		read(emp,fp);
-	}
-	
-	else 
-		exit(0);
-	*/
-	FILE *fp;
-	struct employee *emp;
-	input(emp,fp);
-	read(emp,fp);
-}	
 
+	if (choice == 2)
+	{
+		read(&emp,fp);
+	}
+
+	else
+		exit(0);
+
+	//FILE *fp;
+	//struct employee *emp;
+	//input(emp,fp);
+	//read(emp,fp);
+}
---
+++ test.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main()
+{
+  char str1[3],str2[3],str3[5];
+  printf("Enter \n");
+  scanf("%s",str1);
+  scanf("%s",str2);
+  scanf("%s",str3);
+  printf("%s",str1);
+}
